ebf0152adfbff3fded28330d2117438960b3edbe
Pawullon <pawpio97@gmail.com>
2026-02-16 21:08:24 +0100

    capital page slight improvement

--- src/utils/isBranchBuilt.js
+++ src/utils/isBranchBuilt.js
@@ -1,73 +1,77 @@
+const BRANCH_DEPENDENCIES = {
+    g000bb0001: [0, 1, 9, 10],
+    g000bb0002: [1, 9, 10],
+    g000bb0003: [2, 3],
+    g000bb0004: [3],
+    g000bb0005: [4],
+    g000bb0006: [5, 6, 13],
+    g000bb0007: [6],
+    g000bb0008: [7],
+    g000bb0009: [7, 8],
+    g000bb0010: [9],
+    g000bb0011: [10],
+    g000bb0012: [11, 12],
+    g000bb0013: [12],
+    g000bb0014: [13],
+    g000bb0015: [14],
+    g000bb0016: [15],
+    g000bb0017: [16],
+    g000bb0018: [17, 18],
+    g000bb0019: [18],
+    g000bb0020: [19, 20, 21],
+    g000bb0021: [20],
+    g000bb0022: [21],
+    g000bb0023: [22, 23, 29, 30],
+    g000bb0024: [23],
+    g000bb0025: [24, 25, 26, 27, 28],
+    g000bb0026: [25, 26, 27],
+    g000bb0027: [26],
+    g000bb0028: [27],
+    g000bb0029: [28],
+    g000bb0030: [29, 30],
+    g000bb0031: [30],
+    g000bb0032: [31],
+    g000bb0033: [32],
+    g000bb0034: [33],
+    g000bb0035: [34, 35, 47],
+    g000bb0036: [35],
+    g000bb0037: [36],
+    g000bb0038: [37, 38, 39, 40],
+    g000bb0039: [38, 39, 40],
+    g000bb0040: [39],
+    g000bb0041: [40],
+    g000bb0042: [41, 42, 43, 44],
+    g000bb0043: [42, 43, 44],
+    g000bb0044: [43],
+    g000bb0045: [44],
+    g000bb0046: [45, 46],
+    g000bb0047: [46],
+    g000bb0048: [47],
+    g000bb0049: [48],
+    g000bb0050: [49],
+    g000bb0051: [50],
+    g000bb0052: [51],
+    g000bb0053: [52, 53, 54, 55, 56],
+    g000bb0054: [53, 54, 55],
+    g000bb0055: [54],
+    g000bb0056: [55],
+    g000bb0057: [56],
+    g000bb0058: [57, 58, 59],
+    g000bb0059: [58, 59],
+    g000bb0060: [59],
+    g000bb0061: [60, 61],
+    g000bb0062: [61],
+    g000bb0063: [62],
+    g000bb0064: [62, 63, 64],
+    g000bb0065: [64],
+    g000bb0066: [65],
+    g000bb0067: [66],
+    g000bb0068: [67],
+}
+
 export const isBranchBuilt = (buildIndex, builds) => {
-    if (buildIndex === "g000bb0001") return builds[0].isBuilt || builds[1].isBuilt || builds[9].isBuilt || builds[10].isBuilt
-    if (buildIndex === "g000bb0002") return builds[1].isBuilt || builds[9].isBuilt || builds[10].isBuilt
-    if (buildIndex === "g000bb0003") return builds[2].isBuilt || builds[3].isBuilt
-    if (buildIndex === "g000bb0004") return builds[3].isBuilt    
-    if (buildIndex === "g000bb0005") return builds[4].isBuilt
-    if (buildIndex === "g000bb0006") return builds[5].isBuilt || builds[6].isBuilt || builds[13].isBuilt
-    if (buildIndex === "g000bb0007") return builds[6].isBuilt
-    if (buildIndex === "g000bb0008") return builds[7].isBuilt
-    if (buildIndex === "g000bb0009") return builds[7].isBuilt || builds[8].isBuilt
-    if (buildIndex === "g000bb0010") return builds[9].isBuilt
-    if (buildIndex === "g000bb0011") return builds[10].isBuilt
-    if (buildIndex === "g000bb0012") return builds[11].isBuilt || builds[12].isBuilt
-    if (buildIndex === "g000bb0013") return builds[12].isBuilt
-    if (buildIndex === "g000bb0014") return builds[13].isBuilt
-    if (buildIndex === "g000bb0015") return builds[14].isBuilt
-    if (buildIndex === "g000bb0016") return builds[15].isBuilt
-    if (buildIndex === "g000bb0017") return builds[16].isBuilt
+    const dependencies = BRANCH_DEPENDENCIES[buildIndex]
+    if (!dependencies) return false
 
-    if (buildIndex === "g000bb0018") return builds[17].isBuilt || builds[18].isBuilt
-    if (buildIndex === "g000bb0019") return builds[18].isBuilt
-    if (buildIndex === "g000bb0020") return builds[19].isBuilt || builds[20].isBuilt || builds[21].isBuilt
-    if (buildIndex === "g000bb0021") return builds[20].isBuilt
-    if (buildIndex === "g000bb0022") return builds[21].isBuilt
-    if (buildIndex === "g000bb0023") return builds[22].isBuilt || builds[23].isBuilt || builds[29].isBuilt || builds[30].isBuilt
-    if (buildIndex === "g000bb0024") return builds[23].isBuilt
-    if (buildIndex === "g000bb0025") return builds[24].isBuilt || builds[25].isBuilt || builds[26].isBuilt || builds[27].isBuilt || builds[28].isBuilt
-    if (buildIndex === "g000bb0026") return builds[25].isBuilt || builds[26].isBuilt || builds[27].isBuilt
-    if (buildIndex === "g000bb0027") return builds[26].isBuilt
-    if (buildIndex === "g000bb0028") return builds[27].isBuilt
-    if (buildIndex === "g000bb0029") return builds[28].isBuilt
-    if (buildIndex === "g000bb0030") return builds[29].isBuilt || builds[30].isBuilt
-    if (buildIndex === "g000bb0031") return builds[30].isBuilt
-    if (buildIndex === "g000bb0032") return builds[31].isBuilt
-    if (buildIndex === "g000bb0033") return builds[32].isBuilt
-    if (buildIndex === "g000bb0034") return builds[33].isBuilt
-
-    if (buildIndex === "g000bb0035") return builds[34].isBuilt || builds[35].isBuilt || builds[47].isBuilt
-    if (buildIndex === "g000bb0036") return builds[35].isBuilt
-    if (buildIndex === "g000bb0037") return builds[36].isBuilt
-    if (buildIndex === "g000bb0038") return builds[37].isBuilt || builds[38].isBuilt || builds[39].isBuilt || builds[40].isBuilt
-    if (buildIndex === "g000bb0039") return builds[38].isBuilt || builds[39].isBuilt || builds[40].isBuilt
-    if (buildIndex === "g000bb0040") return builds[39].isBuilt
-    if (buildIndex === "g000bb0041") return builds[40].isBuilt
-    if (buildIndex === "g000bb0042") return builds[41].isBuilt || builds[42].isBuilt || builds[43].isBuilt || builds[44].isBuilt
-    if (buildIndex === "g000bb0043") return builds[42].isBuilt || builds[43].isBuilt || builds[44].isBuilt
-    if (buildIndex === "g000bb0044") return builds[43].isBuilt
-    if (buildIndex === "g000bb0045") return builds[44].isBuilt
-    if (buildIndex === "g000bb0046") return builds[45].isBuilt || builds[46].isBuilt
-    if (buildIndex === "g000bb0047") return builds[46].isBuilt
-    if (buildIndex === "g000bb0048") return builds[47].isBuilt
-    if (buildIndex === "g000bb0049") return builds[48].isBuilt
-    if (buildIndex === "g000bb0050") return builds[49].isBuilt
-    if (buildIndex === "g000bb0051") return builds[50].isBuilt
-    
-    if (buildIndex === "g000bb0052") return builds[51].isBuilt
-    if (buildIndex === "g000bb0053") return builds[52].isBuilt || builds[53].isBuilt || builds[54].isBuilt || builds[55].isBuilt || builds[56].isBuilt
-    if (buildIndex === "g000bb0054") return builds[53].isBuilt || builds[54].isBuilt || builds[55].isBuilt
-    if (buildIndex === "g000bb0055") return builds[54].isBuilt
-    if (buildIndex === "g000bb0056") return builds[55].isBuilt
-    if (buildIndex === "g000bb0057") return builds[56].isBuilt
-    if (buildIndex === "g000bb0058") return builds[57].isBuilt || builds[58].isBuilt || builds[59].isBuilt
-    if (buildIndex === "g000bb0059") return builds[58].isBuilt || builds[59].isBuilt
-    if (buildIndex === "g000bb0060") return builds[59].isBuilt
-    if (buildIndex === "g000bb0061") return builds[60].isBuilt || builds[61].isBuilt
-    if (buildIndex === "g000bb0062") return builds[61].isBuilt
-    if (buildIndex === "g000bb0063") return builds[62].isBuilt
-    if (buildIndex === "g000bb0064") return builds[62].isBuilt || builds[63].isBuilt || builds[64].isBuilt
-    if (buildIndex === "g000bb0065") return builds[64].isBuilt
-    if (buildIndex === "g000bb0066") return builds[65].isBuilt
-    if (buildIndex === "g000bb0067") return builds[66].isBuilt
-    if (buildIndex === "g000bb0068") return builds[67].isBuilt
+    return dependencies.some(index => builds[index]?.isBuilt)
 }
